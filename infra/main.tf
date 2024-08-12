e7a8365cb985c78e1395f0c68ca09deb9c31100b
Agustín Vargas Toro <fustincho@tuta.io>
2024-08-12 10:27:48 -0500

    update: add backend definition

--- infra/main.tf
+++ infra/main.tf
@@ -5,3 +5,12 @@ provider "aws" {
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "fustincho-tf-gha-test"  
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "fustincho-infra"
+    key            = "bucket_test/terraform.tfstate"  # Path to the state file in the bucket
+    region         = "us-east-1"                  
+    encrypt        = true                             # Optional: Encrypt the state file at rest
+  }
+}
